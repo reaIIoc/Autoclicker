40a830da7e7d21e77e430cadd972165543303c9a
datarec <73265375+datarec@users.noreply.github.com>
2024-12-21 17:33:47 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,5 +1,11 @@
 import subprocess
+import time
 
 subprocess.run(["cls"], shell=True)
 subprocess.run(["pip", "install", "pywin32"], shell=True, capture_output=True)
 subprocess.run(["pip", "install", "pydirectinput"], shell=True, capture_output=True)
+
+print("Installing dependencies...")
+time.sleep(0.5)
+print("\nInstallation and setup complete!")
+
