3e0d58ce2a64c7e060df50f4780ea5cc63cd28c4
datarec <73265375+datarec@users.noreply.github.com>
2024-12-21 17:18:17 +1100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,4 @@
 import subprocess
 
-subprocess.run(["pip", "install", "pywin32"], shell=True)
-subprocess.run(["pip", "install", "pydirectinput"], shell=True)
+subprocess.run(["pip", "install", "pywin32"], shell=True, capture_output=True)
+subprocess.run(["pip", "install", "pydirectinput"], shell=True, capture_output=True)
